f0a365b6d06a6db5dfb80faf2c6fcf88624944f6
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-08-31 21:36:39 +0530

    New updates

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,10 +4,12 @@ import './style.css'
 function NavBar() {
     return (
         <nav>
-            <div className="nav__logo">SkyEase</div>
+            <div className="nav__logo">
+                SkyEase
+            </div>
             <ul className="nav__links">
-                <li className="link"><a href="#">Home</a></li>
-                <li className="link"><a href="#">About</a></li>
+                <li className="link"><a href="./">Home</a></li>
+                <li className="link"><a href="/about">About</a></li>
                 <li className="link"><a href="#">Offers</a></li>
                 <li className="link"><a href="#">Seats</a></li>
                 <li className="link"><a href="#">Destinations</a></li>
