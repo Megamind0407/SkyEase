6ebfcd67ad9734427f1204d281efe19cf3a734bd
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-16 21:51:55 +0530

    add about route

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
-import Header from './Header'
+import Header from './Header';
 import BookingForm from './BookingForm';
 import PlanSection from './PlanSection';
-import Memories from './Memories'
-import Lounge from './Lounge'
-import Travellers from './Travellers'
-import Subscribe from './Subscribe'
+import Memories from './Memories';
+import Lounge from './Lounge';
+import Travellers from './Travellers';
+import Subscribe from './Subscribe';
 import Footer from './Footer';
-import About from './Components/About'
+import About from './Components/About/About';
+
 function App() {
+  const location = useLocation(); 
+
   return (
     <div className="App">
       <Navbar />
-      <Header />
-      <BookingForm />
-      <PlanSection />
-      <Memories />
-      <Lounge />
-      <Travellers />
-      <Subscribe />
+      {location.pathname === '/' && (
+        <>
+          <Header />
+          <BookingForm />
+          <PlanSection />
+          <Memories />
+          <Lounge />
+          <Travellers />
+          <Subscribe />
+        </>
+      )}
+      <Routes>
+        <Route path="/About" element={<About />} />
+      </Routes>
       <Footer />
-          <Routes>
-            <Route path='/About' element={<About />} />
-          </Routes>
     </div>
   );
 }
