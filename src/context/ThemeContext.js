ed142be7568480a6301007b841790780e03e4787
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-16 21:55:13 +0530

    add dark mode

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,20 @@
+// src/context/ThemeContext.js
+import { createContext, useState, useContext } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleTheme = () => {
+        setIsDarkMode((prevMode) => !prevMode);
+    };
+
+    return (
+        <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => useContext(ThemeContext);
