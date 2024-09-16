1ee76a43632c864a633e6391badc0f2e5b6475ae
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-16 21:52:45 +0530

    Update header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import header from './assets/header.jpg'
+import header from './assets/airline_header.jpg'
 import './style.css'
 
 function Header() {
     return (
-        <header className="section__container header__container">
-            <h1 className="section__header">Find And Book<br />A Great Experience</h1>
-            <img src={header} alt="header" />
+        <header className="header__container" >
+            <img src={header} alt="header"/>
+            <h1 className="header_text">Find And Book<br />A Great Experience</h1>
         </header>
     );
 }
