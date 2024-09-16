1978bd7b95952ec52fefad4d284ad34da84e3887
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-16 22:49:55 +0530

    remove dark theme

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { ThemeProvider } from './context/ThemeContext';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
-    <ThemeProvider>
       <App />
-    </ThemeProvider>
   </BrowserRouter>
 );
