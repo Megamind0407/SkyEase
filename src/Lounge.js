c46ead1c55cb4ccaefa31ff814a34fe599aa672b
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-25 01:13:41 +0530

    update lounge

--- src/Lounge.js
+++ src/Lounge.js
@@ -6,11 +6,11 @@ function LoungeSection() {
     return (
         <section className="section__container lounge__container">
             <div className="lounge__image">
-                <img src={lounge1} alt="lounge" />
                 <img src={lounge2}alt="lounge" />
+                <img src={lounge1} alt="lounge" />
             </div>
             <div className="lounge__content">
-                <h2 className="section__header">Unaccompanied Minor Lounge</h2>
+                <h2 className="section__header">Premium Lounge Experience</h2>
                 <div className="lounge__grid">
                     <div className="lounge__details">
                         <h4>Experience Tranquility</h4>
