8a942c6b868884cb69ea9f03899af711be1f20df
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-25 23:34:08 +0530

    update content

--- src/PlanSection.js
+++ src/PlanSection.js
@@ -7,26 +7,26 @@ function PlanSection() {
     return (
         <section className="section__container plan__container">
             <p className="subheader">TRAVEL SUPPORT</p>
-            <h2 className="section__header">Plan your travel with confidence</h2>
+            <h2 className="section__header">Prepare your trip with assurance.</h2>
             <p className="description">
-                Find help with your bookings and travel plans, and see what to expect along your journey.
+                See what to expect on your trip and get assistance with bookings and travel arrangements.
             </p>
             <div className="plan__grid">
                 <div className="plan__content">
                     <span className="number">01</span>
-                    <h4>Travel Requirements for Dubai</h4>
+                    <h4>Travel Requirements</h4>
                     <p>
-                        Stay informed and prepared for your trip to Dubai with essential travel requirements, ensuring a smooth and hassle-free experience in this vibrant and captivating city.
+                        To ensure a hassle-free and seamless visit to this fascinating and lively city, be well-informed and organized for your trip by knowing the necessary travel needs.
                     </p>
                     <span className="number">02</span>
-                    <h4>Multi-risk travel insurance</h4>
+                    <h4>Travel insurance with several risks</h4>
                     <p>
-                        Comprehensive protection for your peace of mind, covering a range of potential travel risks and unexpected situations.
+                        Entire peace of mind protection, encompassing a variety of possible travel hazards and unforeseen circumstances.
                     </p>
                     <span className="number">03</span>
-                    <h4>Travel Requirements by destinations</h4>
+                    <h4>Destination-specific Travel Requirements</h4>
                     <p>
-                        Stay informed and plan your trip with ease, as we provide up-to-date information on travel requirements specific to your desired destinations.
+                        We offer the most recent information on travel needs unique to your targeted places, so stay informed and organize your trip with ease.
                     </p>
                 </div>
                 <div className="plan__image">
