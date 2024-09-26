1ea2ba3b34c88fb9b5c23d795c3802c530778361
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-26 14:11:51 +0530

    changes commited in nabar

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,7 @@ function NavBar() {
                 SkyEase
             </div>
             <ul className="nav__links">
-                <li className="link"><a href="./">Home</a></li>
+                <li className="link"><a href="/">Home</a></li>
                 <li className="link"><a href="/about">About</a></li>
                 <li className="link"><a href="#">Offers</a></li>
                 <li className="link"><a href="#">Seats</a></li>
